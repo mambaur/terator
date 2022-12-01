520a3b7b9d60c80c003af5c1e7d01c5c39b49b29
mambaur <alwiroziq1998@gmail.com>
2022-12-02 00:55:06 +0700

    update flutter quill

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,35 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:delta_to_html/delta_to_html.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Terator'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,67 +37,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final quill.QuillController _controller = quill.QuillController.basic();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  convert(String htmlData, String name) async {
+    // if (!await Permission.contacts.request().isGranted) {
+    //   print('Request failed');
+    //   return;
+    // }
+
+    // Name is File Name that you want to give the file
+    var targetPath = await _localPath;
+    var targetFileName = name;
+
+    var generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(
+        htmlData, targetPath!, targetFileName);
+    print(generatedPdfFile);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(generatedPdfFile.toString()),
+    ));
+  }
+
+  Future<String?> get _localPath async {
+    Directory? directory;
+    try {
+      if (Platform.isIOS) {
+        directory = await getApplicationSupportDirectory();
+      } else {
+        // if platform is android
+        // if (await Permission.storage.request().isGranted) {
+        //   directory = Directory('/storage/emulated/0/Download');
+        //   if (!await directory.exists()) {
+        //     directory = await getExternalStorageDirectory();
+        //   }
+        // }
+
+        directory = Directory('/storage/emulated/0/Download');
+        if (!await directory.exists()) {
+          directory = await getExternalStorageDirectory();
+        }
+
+        // directory = await getTemporaryDirectory();
+        // directory = await getExternalStorageDirectory();
+      }
+    } catch (err, stack) {
+      print("Can-not get download folder path");
+    }
+    return directory?.path;
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: () {
+                // print(_controller.document.toDelta());
+                quill.Delta delta = _controller.document.toDelta();
+                // print(delta.toJson());
+
+                String htmlData = DeltaToHTML.encodeJson(delta.toJson());
+                convert(htmlData, "PDF${Random().nextInt(1000)}");
+                print(htmlData);
+              },
+              icon: Icon(Icons.save))
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Card(
+            child: quill.QuillToolbar.basic(
+              // toolbarSectionSpacing: 0,
+              // toolbarIconAlignment: WrapAlignment.spaceEvenly,
+              multiRowsDisplay: false,
+              showHeaderStyle: true,
+              toolbarIconSize: 25,
+
+              controller: _controller,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: quill.QuillEditor.basic(
+                controller: _controller,
+                readOnly: false, // true for view only mode
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          )
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
