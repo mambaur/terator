39f75901721fcfe1265996a929fca143d3cc0f30
mambaur <alwiroziq1998@gmail.com>
2022-12-05 11:32:02 +0700

    add letter repository and update account repository

--- lib/repositories/account_repository.dart
+++ lib/repositories/account_repository.dart
@@ -1,5 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:terator/core/database.dart';
+import 'package:terator/models/account_model.dart';
 
 class AccountRepository {
   // reference to our single class that manages the database
@@ -8,6 +9,56 @@ class AccountRepository {
   Future<int> insert(Map<String, dynamic> row) async {
     Database db = await dbInstance.init;
     return await db.insert(dbInstance.accountTable, row);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await dbInstance.init;
+    int id = row[dbInstance.accountId];
+    return await db.update(dbInstance.accountTable, row,
+        where: '${dbInstance.accountId} = ?', whereArgs: [id]);
+  }
+
+  Future<List<AccountModel>> all({int? limit, int? page}) async {
+    try {
+      // Setup pagination
+      limit ??= 10;
+      int offset = (limit * ((page ?? 0) + 1)) - limit;
+
+      Database db = await dbInstance.init;
+      final data = await db.rawQuery(
+          'SELECT *  FROM ${dbInstance.accountTable} ORDER BY ${dbInstance.accountTable}.${dbInstance.accountUpdatedAt} DESC LIMIT $limit OFFSET $offset',
+          []);
+
+      List<AccountModel> listAccounts = [];
+      if (data.isNotEmpty) {
+        for (var i = 0; i < data.length; i++) {
+          AccountModel accountModel = AccountModel(
+            id: int.parse(data[i]['id'].toString()),
+            name: data[i]['name'].toString(),
+            placeAndDateOfBirth: data[i]['place_and_date_of_birth'].toString(),
+            gender: data[i]['gender'].toString(),
+            religion: data[i]['religion'].toString(),
+            lastEducation: data[i]['last_education'].toString(),
+            educationClass: data[i]['education_class'].toString(),
+            educationInstitution: data[i]['education_institution'].toString(),
+            heightOrWeight: data[i]['height_or_weight'].toString(),
+            telephone: data[i]['telephone'].toString(),
+            email: data[i]['email'].toString(),
+            maritalStatus: data[i]['marital_status'].toString(),
+            address: data[i]['address'].toString(),
+            signatureImage: data[i]['signature_image'].toString(),
+            letterCityWritten: data[i]['letter_city_written'].toString(),
+            createdAt: data[i]['created_at'].toString(),
+            updatedAt: data[i]['updated_at'].toString(),
+          );
+          listAccounts.add(accountModel);
+        }
+      }
+
+      return listAccounts;
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Future<int> delete(int id) async {
---
+++ lib/repositories/letter_repository.dart
@@ -0,0 +1,59 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:terator/core/database.dart';
+import 'package:terator/models/letter_model.dart';
+
+class LetterRepository {
+  // reference to our single class that manages the database
+  final dbInstance = DatabaseInstance();
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await dbInstance.init;
+    return await db.insert(dbInstance.letterTable, row);
+  }
+
+  Future<int> update(Map<String, dynamic> row) async {
+    Database db = await dbInstance.init;
+    int id = row[dbInstance.letterId];
+    return await db.update(dbInstance.letterTable, row,
+        where: '${dbInstance.letterId} = ?', whereArgs: [id]);
+  }
+
+  Future<List<LetterModel>> all({int? limit, int? page}) async {
+    try {
+      // Setup pagination
+      limit ??= 10;
+      int offset = (limit * ((page ?? 0) + 1)) - limit;
+
+      Database db = await dbInstance.init;
+      final data = await db.rawQuery(
+          'SELECT *  FROM ${dbInstance.letterTable} ORDER BY ${dbInstance.letterTable}.${dbInstance.letterUpdatedAt} DESC LIMIT $limit OFFSET $offset',
+          []);
+
+      List<LetterModel> listLetters = [];
+      if (data.isNotEmpty) {
+        for (var i = 0; i < data.length; i++) {
+          LetterModel letterModel = LetterModel(
+            id: int.parse(data[i]['id'].toString()),
+            accountId: int.parse(data[i]['account_id'].toString()),
+            name: data[i]['name'].toString(),
+            html: data[i]['html'].toString(),
+            withSignature: data[i]['with_signature'].toString(),
+            createdAt: data[i]['created_at'].toString(),
+            updatedAt: data[i]['updated_at'].toString(),
+          );
+          listLetters.add(letterModel);
+        }
+      }
+
+      return listLetters;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await dbInstance.init;
+    return await db.delete(dbInstance.letterTable,
+        where: '${dbInstance.letterId} = ?', whereArgs: [id]);
+  }
+}
